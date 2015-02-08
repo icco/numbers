b2924bae268dcd54307c40c19a6e48e021047362
Nat Welch <nat@natwelch.com>
2015-02-08 10:32:37 +0000

    -y

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 sudo aptitude update
-sudo aptitude install golang git
+sudo aptitude install -y golang git
 git clone https://github.com/icco/numbers
 cd numbers; go install; sudo ./numbers -p 80
