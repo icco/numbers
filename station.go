df8f6fe1f8ebb93798cc2dfbf5f3c35caef8e1c1
Nat Welch <nat@natwelch.com>
2021-05-08 14:45:39 +0000

    fix lint

--- station.go
+++ station.go
@@ -26,16 +26,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	length := len(text)
 	seconds := float64(time.Hour * 24 * 7)
 	now := time.Now().UTC()
-	seconds_passed := ((int64(now.Nanosecond()) * int64(time.Nanosecond)) +
+	secPassed := ((int64(now.Nanosecond()) * int64(time.Nanosecond)) +
 		(int64(now.Second()) * int64(time.Second)) +
 		(int64(now.Minute()) * int64(time.Minute)) +
 		(int64(now.Hour()) * int64(time.Hour)) +
 		(int64(now.Weekday()) * int64(time.Hour*24)))
 
-	lookup := int64((float64(seconds_passed) / seconds) * float64(length))
+	lookup := int64((float64(secPassed) / seconds) * float64(length))
 	char := rune(text[lookup])
 
-	log.Debugf("(%v / %v) * %d = %d: %s (%d)", seconds_passed, seconds, length, lookup, string(char), char)
+	log.Debugf("(%v / %v) * %d = %d: %s (%d)", secPassed, seconds, length, lookup, string(char), char)
 	fmt.Fprintf(w, "%d", char)
 }
 
